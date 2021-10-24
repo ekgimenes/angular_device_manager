2535cc2bd626918f8b714e826e5ef65d3a78e8a8
DESKTOP-O4T0U8E\erick <ekgimenes@gmail.com>
2021-10-24 13:39:34 -0300

    - Adding @angular/material and @angular/cdk dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
