7bb732976bf5d598bab9453c5fde7a60c8d6df60
DESKTOP-O4T0U8E\erick <ekgimenes@gmail.com>
2021-10-24 13:13:03 -0300

    - Adjusting routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    HomeComponent, 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
